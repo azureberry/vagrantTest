ba6b83c300c9e39e8b532ba253fd3632a2e463b3
azureberry <azureberry@>
2015-11-06 13:05:24 +0900

    build 用

--- chef-repo/recipes/default.rb
+++ chef-repo/recipes/default.rb
@@ -6,3 +6,13 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+
+# # Node.jsのパッケージで必要なものをグローバルインストール
+bash "install npm packages" do
+  cwd "/home/vagrant"
+  user "root"
+  code <<-EOH
+    npm install -g gulp bower
+  EOH
+end
