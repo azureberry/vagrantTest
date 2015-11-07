c5a4bbd3fddc7fa7d4a3c7e3a0d7dd834a261fc5
azureberry <azureberry@>
2015-11-07 21:10:47 +0900

    npm install

--- chef-repo/recipes/default.rb
+++ chef-repo/recipes/default.rb
@@ -7,12 +7,21 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# githubからリポジトリをクローン
+git "/home/vagrant/myweb" do
+    repository "git://github.com/azureberry/myweb.git"
+    user "vagrant"
+    action :checkout
+end
 
-# # Node.jsのパッケージで必要なものをグローバルインストール
+# Node.jsのパッケージで必要なものをグローバルインストール
 bash "install npm packages" do
-  cwd "/home/vagrant"
-  user "root"
+  cwd "/home/vagrant/myweb"
+  user "vagrant"
   code <<-EOH
-    npm install -g gulp bower
+    sudo npm install -g gulp bower
+    bundle install
+    sudo bower install -f --allow-root
+    # sudo npm install
   EOH
 end
