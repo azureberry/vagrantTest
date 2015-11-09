fe509bf16592e7753a7f905948ac829203db7b9b
azureberry <azureberry@>
2015-11-09 22:20:56 +0900

    test以外

--- chef-repo/recipes/default.rb
+++ chef-repo/recipes/default.rb
@@ -7,22 +7,36 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# githubからリポジトリをクローン
-git "/home/vagrant/myweb" do
-    repository "git://github.com/azureberry/myweb.git"
-    user "vagrant"
-    action :checkout
-end
-
 # Node.jsのパッケージで必要なものをグローバルインストール
-bash "install npm packages" do
+bash "install nodejs npm packages" do
   cwd "/home/vagrant"
   user "root"
   code <<-EOH
-    npm install -g gulp bower
-    # bundle install
-    # sudo bower install -f --allow-root
-    # sudo npm install
-    # # sudo npm update
+    curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+    sudo apt-get install -y nodejs
+    sudo npm install -g gulp bower
+  EOH
+end
+
+# githubからリポジトリをクローン
+# git "/home/vagrant/myweb" do
+#     repository "git://github.com/azureberry/myweb.git"
+#     user "vagrant"
+#     group "vagrant"
+#     action :checkout
+# end
+
+# Node.jsのパッケージで必要なものをグローバルインストール
+bash "install myproject" do
+  cwd "/home/vagrant"
+  user "vagrant"
+  group "vagrant"
+  code <<-EOH
+    cp -a /vagrant/myweb .
+    cd myweb
+    bundle install
+    sudo bower install --allow-root
+    sudo npm install
+    node node_modules/protractor/bin/webdriver-manager update
   EOH
 end
