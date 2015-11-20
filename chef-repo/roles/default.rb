0d80fd63b0018e6cc82b46a7d73d15e317822efe
azureberry <azureberry@>
2015-11-20 22:35:53 +0900

    gulp test ok

--- chef-repo/roles/default.rb
+++ chef-repo/roles/default.rb
@@ -1,11 +1,8 @@
-name "default"
-description "Install Oracle Java"
-default_attributes(
-  "java" => {
-    "install_flavor" => "oracle",
-    "jdk_version" => "8",
-    "oracle" => {
-      "accept_oracle_download_terms" => true
-    }
+{
+  "java": {
+    "jdk_version": "7",
+    "install_flavor" : "oracle",
+    "oracle" : { "accept_oracle_download_terms" : "true" },
+    "accept_license_agreement" : "true"
   }
-)
+}
