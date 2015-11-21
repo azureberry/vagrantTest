25d89cf1cf43dda87ce4e9a914443dfc3d48bb19
azureberry <azureberry@>
2015-11-22 04:58:30 +0900

    整理

--- chef-repo/roles/default.rb
+++
@@ -1,8 +0,0 @@
-{
-  "java": {
-    "jdk_version": "7",
-    "install_flavor" : "oracle",
-    "oracle" : { "accept_oracle_download_terms" : "true" },
-    "accept_license_agreement" : "true"
-  }
-}
