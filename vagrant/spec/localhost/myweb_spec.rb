88c2cd72463cf621f97bf8803dde8da6937e5c0b
azureberry <azureberry@>
2015-11-24 12:54:05 +0900

    構造化

--- vagrant/spec/localhost/myweb_spec.rb
+++ vagrant/spec/localhost/myweb_spec.rb
@@ -1,88 +1,95 @@
+# -*- encoding: utf-8 -*-
+
 require 'spec_helper'
 
-pkgs = [
-  'g++',
-  'ruby',
-  'git',
-  'openjdk-7-jdk',
-  'google-chrome-stable',
-  'xvfb',
-  'nodejs',
-  'rake'
-]
+describe 'check packages' do
+  pkgs = [
+    'g++',
+    'ruby',
+    'git',
+    'openjdk-7-jdk',
+    'google-chrome-stable',
+    'xvfb',
+    'nodejs',
+    'rake'
+  ]
 
-for pkg in pkgs do
-  describe package(pkg) do
-    it { should be_installed }
+  pkgs.each do |pkg|
+    describe package(pkg) do
+      it { should be_installed }
+    end
+  end
+
+  pkgs_gem = [
+    'bundler',
+    'sass',
+    'serverspec'
+  ]
+
+  pkgs_gem.each do |pkg|
+    describe package(pkg) do
+      it { should be_installed.by('gem') }
+    end
+  end
+
+  pkgs_npm = [
+    'gulp',
+    'bower'
+  ]
+
+  pkgs_npm.each do |pkg|
+    describe package(pkg) do
+      it { should be_installed.by('npm') }
+    end
+  end
+
+end
+
+describe 'サービス開始されていること' do
+  describe service('xvfb') do
+    it { should be_enabled }
+    # it { should be_running }
   end
 end
-
-pkgs_gem = [
-  'bundler',
-  'sass',
-  'serverspec'
-]
-
-for pkg in pkgs_gem do
-  describe package(pkg) do
-    it { should be_installed.by('gem') }
-  end
-end
-
-pkgs_npm = [
-  'gulp',
-  'bower'
-]
-
-for pkg in pkgs_npm do
-  describe package(pkg) do
-    it { should be_installed.by('npm') }
-  end
-end
-
-# サービス開始されていること
-describe service('xvfb') do
-  it { should be_enabled }
-  # it { should be_running }
-end
-
 
 ################################
 
-#  [install nodejs npm packages]
-# apt-get install nodejs
-#      -> pkgs
-# npm install -g
-#      -> pkgs_npm
-
-
-# [git] gitからリポジトリをチェックアウトされていること
-describe file('/home/vagrant/myweb/') do
-  it { should be_directory }
+describe '[install nodejs npm packages]' do
+  # apt-get install nodejs
+  #      -> pkgs
+  # npm install -g
+  #      -> pkgs_npm
 end
 
-
-# [install myproject] リポジトリ内セットアップが完了していること
-# bundle install
-#      ->pkgs_gem
-
-# bower install
-# npm install
-# node node_modules/protractor/bin/webdriver-manager update
-# build_copy.sh
-files = [
-  '/home/vagrant/myweb/bower_components',
-  '/home/vagrant/myweb/node_modules',
-  '/home/vagrant/myweb/node_modules/protractor/selenium/chromedriver',
-  '/home/vagrant/myweb/src/img'
-]
-for f in files do
-  describe file(f) do
-    it { should exist }
+describe '[git] gitからリポジトリをチェックアウトされていること' do
+  describe file('/home/vagrant/myweb/') do
+    it { should be_directory }
   end
 end
 
-# [install serverspec packages]
-# gem install
-#      -> pkgs_gem
-#      -> pkgs
+describe '[install myproject] リポジトリ内セットアップが完了していること' do
+  # bundle install
+  #      ->pkgs_gem
+
+  # bower install
+  # npm install
+  # node node_modules/protractor/bin/webdriver-manager update
+  # build_copy.sh
+  files = [
+    '/home/vagrant/myweb/bower_components',
+    '/home/vagrant/myweb/node_modules',
+    '/home/vagrant/myweb/node_modules/protractor/selenium/chromedriver',
+    '/home/vagrant/myweb/src/img'
+  ]
+  for f in files do
+    describe file(f) do
+      it { should exist }
+    end
+  end
+end
+
+describe '[install serverspec packages]' do
+  # gem install
+  #      -> pkgs_gem
+  #      -> pkgs
+end
