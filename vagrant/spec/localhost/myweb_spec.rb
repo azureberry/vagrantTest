115f5bcae6e035832098158d232f73e255fdf26e
azureberry <azureberry@>
2015-11-24 09:52:31 +0900

    serverdpec整理

--- vagrant/spec/localhost/myweb_spec.rb
+++ vagrant/spec/localhost/myweb_spec.rb
@@ -1,39 +1,46 @@
 require 'spec_helper'
 
+pkgs = [
+  'g++',
+  'ruby',
+  'git',
+  'openjdk-7-jdk',
+  'google-chrome-stable',
+  'xvfb',
+  'nodejs'
+]
 
-describe package('g++') do
-  it { should be_installed }
+for pkg in pkgs do
+  describe package(pkg) do
+    it { should be_installed }
+  end
 end
 
-describe package('ruby') do
-  it { should be_installed }
+pkgs_gem = [
+  'bundler',
+  'sass',
+  'serverspec',
+  'rake'
+]
+
+for pkg in pkgs_gem do
+  describe package(pkg) do
+    it { should be_installed.by('gem') }
+  end
 end
 
-# describe package('bundler') do
-#   it { should be_installed }
-# end
+pkgs_npm = [
+  'gulp',
+  'bower'
+]
 
-describe package('git') do
-  it { should be_installed }
+for pkg in pkgs_npm do
+  describe package(pkg) do
+    it { should be_installed.by('npm') }
+  end
 end
 
-describe package('openjdk-7-jdk') do
-  it { should be_installed }
-end
-
-describe package('google-chrome-stable') do
-  it { should be_installed }
-end
-
-describe package('xvfb') do
-  it { should be_installed }
-end
-
-describe package('nodejs') do
-  it { should be_installed }
-end
-
-
+# サービス開始されていること
 describe service('xvfb') do
   it { should be_enabled }
   # it { should be_running }
@@ -44,10 +51,24 @@ end
 #   it { should be_listening }
 # end
 
+# gitからリポジトリをチェックアウトされていること
+describe file('/home/vagrant/myweb/') do
+  it { should be_directory }
+end
+
+
+# リポジトリ内セットアップが完了していること
+# bower install
 describe file('/home/vagrant/myweb/bower_components') do
   it { should be_directory }
 end
 
+# npm install
 describe file('/home/vagrant/myweb/node_modules') do
+  it { should be_directory }
+end
+
+# build_copy.sh
+describe file('/home/vagrant/myweb/src/img') do
   it { should be_directory }
 end
